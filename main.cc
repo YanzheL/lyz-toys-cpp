5129d4503cd883bdca49c108bcfbc89ee29ce8f0
YanzheL <lee.yanzhe@yanzhe.org>
2019-03-26 01:18:29 -0700

    update copyright

--- main.cc
+++ main.cc
@@ -1,6 +1,8 @@
-//
+// =======================================================================
+// Copyright (c) 2016-2020 by LI YANZHE. All rights reserved.
+// Author: LI YANZHE <lee.yanzhe@yanzhe.org>
 // Created by LI YANZHE on 3/18/19.
-//
+// =======================================================================
 
 #include "factory/cached_factory.h"
 #include <iostream>
