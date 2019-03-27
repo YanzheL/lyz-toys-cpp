233d28d845a5e8b0763cd4e3bdcbfbd8bb98eb81
YanzheL <lee.yanzhe@yanzhe.org>
2019-03-27 08:09:24 +0800

    test time

--- factory/main.cc
+++ factory/main.cc
@@ -12,6 +12,12 @@ using namespace lyz;
 using SomeHeavyClass = vector<string>;
 
 int main() {
-  auto obj = CachedFactory::createInstance<SomeHeavyClass>(10, "Arg0");
+  shared_ptr<SomeHeavyClass> obj;
+  GETTIME_HIGH(
+      for (int i = 0; i < 10000000; ++i) {
+        obj = CachedFactory::createInstance<SomeHeavyClass>(10, "Arg0");
+      },
+      createInstance
+  )
   cout << (*obj)[0] << endl;
 }
